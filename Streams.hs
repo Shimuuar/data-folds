839843a109ccc2f73130d4d2f788007d7210351e
Aleksey Khudyakov <alexey.skladnoy@gmail.com>
2014-09-20 01:50:17 +0400

    Fold composition

--- Streams.hs
+++ Streams.hs
@@ -13,10 +13,39 @@ import Data.Folds.Monadic
 -- | Infinite stream
 data Stream a = a :> Stream a
 
--- | Convert left fold to stream transformation
+-- | Convert left fold to stream transformation.
+--
+--   N.B. we have two choices. We can either extract first element of
+--   target stream from initial state of fold or only generate new
+--   element by applying step function. It's not yet clear which
+--   choice is correct.
 foldToStream :: Fold a b -> Stream a -> Stream b
 foldToStream (Fold step x out) (a :> xs)
   = out x :> foldToStream (Fold step (step x a) out) xs
+
+-- | We can compose folds.
+composeFold :: Fold a b -> Fold b c -> Fold a c
+composeFold (Fold stepAB xA0 outA) (Fold stepBC xB0 outB)
+  = Fold
+      (\(xA,xB) a -> let xA' = stepAB xA a
+                         b   = outA xA' -- !!!
+                         xB' = stepBC xB b
+                     in (xA',xB')
+      )
+      (xA0,xB0)
+      (\(_,xB) -> outB xB)
+
+-- | We can compose folds.
+composeFold2 :: Fold a b -> Fold b c -> Fold a c
+composeFold2 (Fold stepAB xA0 outA) (Fold stepBC xB0 outB)
+  = Fold
+      (\(xA,xB) a -> let xA' = stepAB xA a
+                         b   = outA xA -- !!!
+                         xB' = stepBC xB b
+                     in (xA',xB')
+      )
+      (xA0,xB0)
+      (\(_,xB) -> outB xB)
 
 
 
